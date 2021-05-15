8cce96257f7d10d40a1e4eee636d608a5fa5be25
Pranay-Pandey <pranaypandeyofficial@gmail.com>
2021-05-15 16:31:26 +0530

    Navigation Drawer

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView Homestudent, Homeadmin;
+    Button vutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        vutton = findViewById(R.id.button);
+        vutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),BackgroundActivity.class));
+            }
+        });
     }
 }
