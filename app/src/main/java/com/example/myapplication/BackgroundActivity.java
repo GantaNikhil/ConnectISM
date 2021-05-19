fc81dc003f9dfbbf67f6fcbaf24e2274f4fd5eac
GantaNikhil <gantanikhil003@gmail.com>
2021-05-19 17:18:10 +0530

    New Request

--- app/src/main/java/com/example/myapplication/BackgroundActivity.java
+++ app/src/main/java/com/example/myapplication/BackgroundActivity.java
@@ -39,8 +39,7 @@ public class BackgroundActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(BackgroundActivity.this,NewRequest.class));
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
